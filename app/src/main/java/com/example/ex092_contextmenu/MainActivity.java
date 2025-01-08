bfb2954ba545ea0a8ca7874f4d173ee326a2bd55
ay4748 <ay4748@bs.amalnet.k12.il>
2025-01-08 20:38:12 +0200

    final!!!!

---
+++ app/src/main/java/com/example/ex092_contextmenu/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.ex092_contextmenu;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean isHeshbonit;
+    Button btnHeshbon,btnHandasa;
+    EditText eTFirst,eTmana;
+    int numFirst,numMana;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        initViews();
+    }
+
+    private void initViews() {
+        btnHeshbon = findViewById(R.id.btnHeshbon);
+        btnHandasa = findViewById(R.id.btnHandasa);
+        eTFirst = findViewById(R.id.eTFirst);
+        eTmana = findViewById(R.id.eTmana);
+    }
+
+    public void heshbon(View view) {
+        isHeshbonit = true;
+        btnHandasa.setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
+        btnHeshbon.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+    }
+
+    public void handasit(View view) {
+        isHeshbonit = false;
+        btnHandasa.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+        btnHeshbon.setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
+    }
+
+    public boolean canAct(String st)
+    {
+        if (st.isEmpty() || st.equals("-") || st.equals(".") || st.equals("+") || st.equals("-.") || st.equals("+.")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    public void nextScreen(View view) {
+        String first = eTFirst.getText().toString();
+        String mana = eTmana.getText().toString();
+        if (!(canAct(first) && canAct(mana)))
+        {
+            Toast.makeText(this, "Invalid input", Toast.LENGTH_LONG).show();
+        }
+
+        Intent si = new Intent(this,MainActivityResult.class);
+        numFirst = Integer.parseInt(first);
+        numMana = Integer.parseInt(mana);
+        si.putExtra("isHeshbonit", isHeshbonit);
+        si.putExtra("numFirst", numFirst);
+        si.putExtra("numMana", numMana);
+        startActivity(si);
+    }
+}
---
+++ app/src/main/java/com/example/ex092_contextmenu/MainActivityResult.java
@@ -0,0 +1,173 @@
+package com.example.ex092_contextmenu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivityResult extends AppCompatActivity implements AdapterView.OnItemLongClickListener,View.OnCreateContextMenuListener,AdapterView.OnItemClickListener {
+
+    ListView lV;
+    TextView tVPos;
+    boolean isHeshbonit;
+    int numFirst, numMana;
+    double[] dodge = new double[20];
+    String[] dodgeString = new String[20];
+    int position;
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_result);
+
+        Intent gi = getIntent();
+        isHeshbonit = gi.getBooleanExtra("isHeshbonit",false);
+
+        numFirst = gi.getIntExtra("numFirst",0);
+        numMana = gi.getIntExtra("numMana",0);
+        tVPos = findViewById(R.id.tVPos);
+        lV = findViewById(R.id.lV);
+
+        lV.setOnItemLongClickListener(this);
+        lV.setOnItemClickListener(this);
+
+        lV.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+
+        if(isHeshbonit)
+        {
+            heshboint(numFirst, numMana);
+        }
+        else
+        {
+            handasit(numFirst,numMana);
+        }
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, dodgeString);
+        lV.setAdapter(adapter);
+        lV.setOnCreateContextMenuListener(this);
+    }
+
+
+    private void heshboint(int numFirst,int numMana)
+    {
+        for(int i = 1; i < 21; i++)
+        {
+            dodge[i-1] = numFirst + (i - 1) * numMana;
+
+        }
+
+        for(int i = 0; i < 20; i++)
+        {
+            if(dodge[i] > 1000000 || dodge[i] < -1000000)
+            {
+                dodgeString[i] = bigNumSimplifier(dodge[i]);
+            }
+            else{
+                dodgeString[i] = String.format("%.4f", dodge[i]);
+            }
+        }
+    }
+
+    private void handasit(int numFirst,int numMana)
+    {
+
+        for(int i = 1; i < 21; i++){
+            dodge[i-1] = numFirst * Math.pow(numMana, i - 1);
+
+        }
+
+        for(int i = 0; i < 20; i++)
+        {
+            if(dodge[i] > 1000000 || dodge[i] < -1000000)
+            {
+                dodgeString[i] = bigNumSimplifier(dodge[i]);
+            }
+            else{
+                dodgeString[i] = String.format("%.4f", dodge[i]);
+            }
+        }
+    }
+
+
+
+
+    public String bigNumSimplifier(double value)
+    {
+        String scientificNotation = String.format("%.4e", value);
+        String[] parts = scientificNotation.split("e");
+        double base = Double.parseDouble(parts[0]) / 10.0;
+        int exponent = Integer.parseInt(parts[1]) + 1;
+
+        return String.format("%.4f * 10^%d", base, exponent);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        menu.add("n");
+        menu.add("Sn");
+    }
+
+    private void initViews() {
+        lV = findViewById(R.id.lV);
+    }
+
+    public void back(View view) {
+        finish();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int pos, long id)
+    {
+        position = pos ;
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int pos, long id) {
+        position = pos ;
+
+        return false;
+    }
+
+    public boolean onContextItemSelected (MenuItem item)
+    {
+
+        String Do = item.getTitle().toString();
+
+        if(Do.equals("n"))
+        {
+            position++;
+            tVPos.setText(position + "");
+        }
+        else if(Do.equals("Sn"))
+        {
+            int sum =  0;
+            for(int i = 0; i <= position ; i++)
+            {
+                sum += dodge[i];
+            }
+            tVPos.setText(sum + "");
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+}
